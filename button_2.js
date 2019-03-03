b891abff764543826a30c83c5b046d3683ff9658
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-03 13:09:08 -0300

    fixed svg circle button

--- button_2.js
+++ button_2.js
@@ -3,13 +3,11 @@ let button2 = function(){
 
   let length = bts.length
   for (let i =0;i<length;i++){
-    bts[i].addEventListener('mousemove', function(e){
+    bts[i].addEventListener('click', function(e){
       let circle = this.getElementsByTagName('circle')[0]
-      circle.style.cx = e.clientX - this.offsetLeft
-      circle.style.cy = e.clientY - this.offsetTop
-    })
-    bts[i].addEventListener('click', function(){
-      let circle = this.getElementsByTagName('circle')[0]
+      let rect = e.target.getBoundingClientRect()
+      circle.style.cx = e.clientX - rect.left
+      circle.style.cy = e.clientY - rect.top
       circle.classList.add('button-2-animation')
       setTimeout(function(){
         circle.classList.remove('button-2-animation')
